3fbd0a346f02009205515169843d4f1eaedb5d76
adair_chan <adair_chan@hotmail.com>
2019-11-11 22:50:32 +0800

    feat(Kafka): set config of Producer

--- kafka/src/main/java/com/cjw/bigdata/kafka/config/KafkaProducerConfig.java
+++ kafka/src/main/java/com/cjw/bigdata/kafka/config/KafkaProducerConfig.java
@@ -2,27 +2,41 @@ package com.cjw.bigdata.kafka.config;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import com.cjw.bigdata.kafka.common.MessageEntity;
 
 /**
  * @author Ego
  * @version 1.0
  * @since 2019/11/11 10:30 上午
  */
-
+@Configuration
 public class KafkaProducerConfig {
 
+    @Value("${kafka.producer.servers}")
     private String servers;
+    @Value("${kafka.producer.retries}")
     private int retries;
+    @Value("${kafka.producer.batch.size}")
     private int batchSize;
+    @Value("${kafka.producer.linger}")
     private int linger;
+    @Value("${kafka.producer.buffer.memory}")
     private int bufferMemory;
 
 
-
-    public Map<String, Object> producerConfigs(){
-        Map<String, Object> props = new HashMap<String, Object>();
+    private Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
         props.put(ProducerConfig.RETRIES_CONFIG, retries);
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, batchSize);
@@ -33,5 +47,15 @@ public class KafkaProducerConfig {
         return props;
     }
 
+    private ProducerFactory<String, MessageEntity> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs(),
+                new StringSerializer(),
+                new JsonSerializer<>());
+    }
+
+    @Bean
+    public KafkaTemplate<String, MessageEntity> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 
 }
