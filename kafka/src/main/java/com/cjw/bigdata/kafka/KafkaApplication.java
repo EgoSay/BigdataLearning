e5255af6f8bbc7d19c9ce36ebececbe7df01b42f
adair_chan <adair_chan@hotmail.com>
2019-11-11 23:04:03 +0800

    feat(Kafka): create SpringBoot start Application

---
+++ kafka/src/main/java/com/cjw/bigdata/kafka/KafkaApplication.java
@@ -0,0 +1,18 @@
+package com.cjw.bigdata.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ego
+ * @version 1.0
+ * @since 2019/11/11 4:02 下午
+ */
+
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+}
