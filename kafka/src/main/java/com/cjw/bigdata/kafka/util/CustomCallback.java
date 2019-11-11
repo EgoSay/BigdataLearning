a5341349a8abe2eceabe937d5a809d54a2858b7f
adair_chan <adair_chan@hotmail.com>
2019-11-11 22:53:59 +0800

    feat(Kafka): create a custom CallBack function

---
+++ kafka/src/main/java/com/cjw/bigdata/kafka/util/CustomCallback.java
@@ -0,0 +1,64 @@
+package com.cjw.bigdata.kafka.util;
+
+import com.cjw.bigdata.kafka.common.MessageEntity;
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.text.SimpleDateFormat;
+
+/**
+ * 自定义异步回调处理类
+ * @author Ego
+ * @version 1.0
+ * @since 2019/11/11 3:07 下午
+ */
+
+@Slf4j
+public class CustomCallback implements ListenableFutureCallback<SendResult<String, MessageEntity>> {
+
+    private final long startTime;
+    private final String key;
+    private final MessageEntity message;
+
+    private final Gson gson = new Gson();
+
+    public CustomCallback(long startTime, String key, MessageEntity message) {
+        this.startTime = startTime;
+        this.key = key;
+        this.message = message;
+    }
+
+    @Override
+    public void onSuccess(@Nullable SendResult<String, MessageEntity> result) {
+        if (result == null) {
+            return;
+        }
+        long elapsedTime = System.currentTimeMillis() - startTime;
+
+        RecordMetadata metadata = result.getRecordMetadata();
+        if (metadata != null) {
+            StringBuilder record = new StringBuilder();
+            log.info("处理Kafka消息");
+            record.append("message(")
+                    .append("key = ").append(key).append(",")
+                    .append("message = ").append(gson.toJson(message)).append(")")
+                    .append("sent to partition(").append(metadata.partition()).append(")")
+                    .append("with offset(").append(metadata.offset()).append(")")
+                    .append("in ").append(elapsedTime).append(" ms");
+            log.info(record.toString());
+        }
+    }
+
+    @Override
+    public void onFailure(Throwable ex) {
+        ex.printStackTrace();
+    }
+
+
+}
