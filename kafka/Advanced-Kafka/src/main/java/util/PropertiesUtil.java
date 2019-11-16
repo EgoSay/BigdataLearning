129460a2a6c911759e60df05d9679122ff0458fd
adair_chan <adair_chan@hotmail.com>
2019-11-16 18:50:22 +0800

    feat(Kafka): Kafka进阶
    工具类

---
+++ kafka/Advanced-Kafka/src/main/java/util/PropertiesUtil.java
@@ -0,0 +1,36 @@
+package util;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * @author Ego
+ * @version 1.0
+ * @since 2019/11/15 3:41 下午
+ */
+
+public class PropertiesUtil {
+
+    private static final String brokerList ="hadoop2:6667";
+
+    public  Properties initConsumerProperties() {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        return properties;
+    }
+
+    public  Properties initProducerProperties() {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerList);
+        return properties;
+    }
+}
