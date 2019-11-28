0c2d7fa6891daddc7baa6e998a3a54651d389a5e
adair_chan <adair_chan@hotmail.com>
2019-11-28 16:49:23 +0800

    feat(Kafka): Kafka 进阶
    Exact-once 语义实现：利用文件存储保存 offset 偏移量

--- kafka/Advanced-Kafka/src/main/java/transaction/OffsetManagerWithFileStore.java
+++ kafka/Advanced-Kafka/src/main/java/transaction/OffsetManagerWithFileStore.java
@@ -1,5 +1,13 @@
 package transaction;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * 利用文件存储保存 offset 偏移量
  * @author Ego
@@ -7,4 +15,47 @@ package transaction;
  * @since 2019/11/27 18:48
  */
 public class OffsetManagerWithFileStore {
+
+    private String storagePrefix;
+
+
+    public OffsetManagerWithFileStore(String storagePrefix) {
+        this.storagePrefix = storagePrefix;
+    }
+
+    /**
+     * 将 offset 保存到外部文件数据源
+     * @param topic: topic
+     * @param partition: partition
+     * @param offset: offset
+     */
+    public void saveOffsetToExternalStore(String topic, int partition, long offset) {
+        try {
+
+            FileWriter fileWriter = new FileWriter(storageName(topic, partition), false);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(offset + "");
+            bufferedWriter.flush();
+            bufferedWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public long readOffsetFromExternalStore(String topic, int partition) {
+        try {
+            Stream<String> stream = Files.lines(Paths.get(storageName(topic, partition)));
+            return Long.parseLong(stream.collect(Collectors.toList()).get(0) + 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+
+    }
+
+    private String storageName(String topic, int partition) {
+        return storagePrefix + "-" + topic + "-" + partition;
+    }
 }
