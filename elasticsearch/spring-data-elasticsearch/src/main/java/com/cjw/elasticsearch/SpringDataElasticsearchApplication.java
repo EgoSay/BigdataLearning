14ecfe95ae8b85c068d524111e3bd5d3eca39ff9
adair_chan <adair_chan@hotmail.com>
2020-06-16 14:12:36 +0800

    build(elasticsearch): update project structure

--- elasticsearch/spring-data-elasticsearch/src/main/java/com/cjw/elasticsearch/SpringDataElasticsearchApplication.java
+++ elasticsearch/spring-data-elasticsearch/src/main/java/com/cjw/elasticsearch/SpringDataElasticsearchApplication.java
@@ -3,6 +3,9 @@ package com.cjw.elasticsearch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Ego
+ */
 @SpringBootApplication
 public class SpringDataElasticsearchApplication {
 
