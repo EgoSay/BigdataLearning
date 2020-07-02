4d190651dcba4b5d4e533e0593221edd1e975f36
adair_chan <adair_chan@hotmail.com>
2020-07-02 18:17:40 +0800

    feat(elasticsearch): help to get High Level REST Client
    help to get High Level REST Client

--- elasticsearch/spring-data-elasticsearch/src/main/java/com/cjw/elasticsearch/common/RestClientHelper.java
+++ elasticsearch/spring-data-elasticsearch/src/main/java/com/cjw/elasticsearch/common/RestClientHelper.java
@@ -1,12 +1,11 @@
 package com.cjw.elasticsearch.common;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHost;
-import org.apache.log4j.Logger;
 import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
 /**
@@ -15,42 +14,32 @@ import org.springframework.util.StringUtils;
  * @date 2020/6/16 11:39
  * @since JDK1.8
  */
-@Configuration
+@Slf4j
+@Component
 public class RestClientHelper {
 
-    @Value("${elasticsearch.cluster-nodes}")
+    @Value("${spring.elasticsearch.bboss.elasticsearch.rest.hostNames}")
     private String clusterNodes;
-    @Value("${elasticsearch.cluster-name}")
-    private String clusterName;
-    @Value("${elasticsearch.request-type}")
-    private String requestType;
 
-    private static final Logger log = Logger.getLogger(RestClientHelper.class);
     private volatile static RestHighLevelClient client;
 
-    public RestClientHelper() {
-        if (!StringUtils.isEmpty(clusterNodes)) {
-            String[] nodes = clusterNodes.split(",");
-            HttpHost[] hosts = new HttpHost[nodes.length];
-
-            for (int i = 0; i < nodes.length; i++) {
-                String[] nodeInfo = nodes[i].split(":");
-                HttpHost httpHost = new HttpHost(nodeInfo[0], Integer.parseInt(nodeInfo[1]), requestType);
-                hosts[i] = httpHost;
-            }
-
-            RestClientBuilder clientBuilder = RestClient.builder(hosts);
-            client = new RestHighLevelClient(clientBuilder);
-        }
-    }
-
-
-    public static RestHighLevelClient getClient() {
+    public  RestHighLevelClient getClient() {
         try {
             if (client == null) {
                 synchronized (RestClientHelper.class) {
                     if (client == null) {
-                        RestClientHelper restClientHelper = new RestClientHelper();
+                        if (!StringUtils.isEmpty(clusterNodes)) {
+                            String[] nodes = clusterNodes.split(",");
+                            HttpHost[] hosts = new HttpHost[nodes.length];
+
+                            for (int i = 0; i < nodes.length; i++) {
+                                String[] nodeInfo = nodes[i].split(":");
+                                HttpHost httpHost = new HttpHost(nodeInfo[0], Integer.parseInt(nodeInfo[1]));
+                                hosts[i] = httpHost;
+                            }
+
+                            client = new RestHighLevelClient(RestClient.builder(hosts));
+                        }
                     }
                 }
             }
